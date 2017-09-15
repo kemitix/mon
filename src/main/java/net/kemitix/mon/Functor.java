b1066bb4b0444722c9199e210bb7b9399061742a
Paul Campbell <pcampbell@kemitix.net>
2017-09-15 19:13:11 +0100

    Functor<T>

---
+++ src/main/java/net/kemitix/mon/Functor.java
@@ -0,0 +1,44 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017 Paul Campbell
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies
+ * or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+ * AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package net.kemitix.mon;
+
+import java.util.function.Function;
+
+/**
+ * The Functor interface.
+ *
+ * @param <T> the type of the functor content
+ *
+ * @author Tomasz Nurkiewicz (?@?.?)
+ */
+public interface Functor<T> {
+
+    /**
+     * Map the content of the functor through the function.
+     *
+     * @param f   the function
+     * @param <R> the type the functor maps to
+     *
+     * @return the new functor
+     */
+    <R> Functor<R> map(Function<T, R> f);
+}
