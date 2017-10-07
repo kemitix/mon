4c85e56c803612faed22b63e2617be799d6e879c
Paul Campbell <pcampbell@kemitix.net>
2017-10-07 18:56:38 +0100

    TypeAlias.map(): added

--- src/main/java/net/kemitix/mon/TypeAlias.java
+++ src/main/java/net/kemitix/mon/TypeAlias.java
@@ -21,6 +21,8 @@
 
 package net.kemitix.mon;
 
+import java.util.function.Function;
+
 /**
  * Type Alias for other types.
  *
@@ -43,6 +45,19 @@ public abstract class TypeAlias<T> {
      */
     protected TypeAlias(final T value) {
         this.value = value;
+    }
+
+
+    /**
+     * Map the TypeAlias into another value.
+     *
+     * @param f   the function to create the new value
+     * @param <R> the type of the new value
+     *
+     * @return a TypeAlias
+     */
+    public <R> R map(final Function<T, R> f) {
+        return f.apply(value);
     }
 
     @Override
@@ -71,5 +86,4 @@ public abstract class TypeAlias<T> {
     public final T getValue() {
         return value;
     }
-
 }
