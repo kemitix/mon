470cd2fc242a6d8e3409e1bccf9bc93e6ac801f8
Paul Campbell <pcampbell@kemitix.net>
2018-06-21 17:52:07 +0100

    Add Result<T>

---
+++ src/main/java/net/kemitix/mon/result/Result.java
@@ -0,0 +1,90 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017 Paul Campbell
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies
+ * or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+ * AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package net.kemitix.mon.result;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+/**
+ * An Either type for holding a result or an error (exception).
+ *
+ * @param <T> the type of the result when a success
+ *
+ * @author Paul Campbell (pcampbell@kemitix.net)
+ */
+public interface Result<T> {
+
+    /**
+     * Create a Result for a success.
+     *
+     * @param value the value
+     * @param <T> the type of the value
+     * @return a successful Result
+     */
+    static <T> Result<T> ok(final T value) {
+        return new Success<T>(value);
+    }
+
+    /**
+     * Create a Result for an error.
+     *
+     * @param error the error (exception)
+     * @param <T> the type had the result been a success
+     * @return an error Result
+     */
+    static <T> Result<T> error(final Throwable error) {
+        return new Err<T>(error);
+    }
+
+    /**
+     * Checks of the Result is an error.
+     *
+     * @return true if the Result is an error.
+     */
+    boolean isError();
+
+    /**
+     * Checks of the Result is a success.
+     *
+     * @return true if the Result is a success.
+     */
+    boolean isOkay();
+
+    /**
+     * Returns a new Result consisting of the result of applying the function to the contents of the Result.
+     *
+     * @param f   the mapping function the produces a Result
+     * @param <R> the type of the result of the mapping function
+     *
+     * @return a Result
+     */
+    <R> Result<R> flatMap(Function<T, Result<R>> f);
+
+    /**
+     * Matches the Result, either success or error, and supplies the appropriate Consumer with the value or error.
+     *
+     * @param onSuccess the Consumer to pass the value of a successful Result to
+     * @param onError the Consumer to pass the error from an error Result to
+     */
+    void match(Consumer<T> onSuccess, Consumer<Throwable> onError);
+
+}
