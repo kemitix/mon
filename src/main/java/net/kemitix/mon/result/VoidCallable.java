73b54949425b5236e476d0fe77d1dca8f9217791
Paul Campbell <pcampbell@kemitix.net>
2021-03-21 19:19:50 +0000

    Add missing javadoc for result.VoidCallable

--- src/main/java/net/kemitix/mon/result/VoidCallable.java
+++ src/main/java/net/kemitix/mon/result/VoidCallable.java
@@ -1,6 +1,16 @@
 package net.kemitix.mon.result;
 
+/**
+ * A task that returns void and may throw an exception.
+ *
+ * <p>Implementors define a single method with no arguments called call.
+ * The VoidCallable interface is similar to Callable, but does not return a value.</p>
+ */
 @FunctionalInterface
 public interface VoidCallable {
+    /**
+     * Executes and may throw an exception.
+     */
+    @SuppressWarnings("PMD.SignatureDeclareThrowsException")
     void call() throws Exception;
 }
