d0d1163dcd439dd45d5a15f92605e67952f4ec82
Paul Campbell <pcampbell@kemitix.net>
2021-08-18 07:23:41 +0100

    Reader: add API status annotations

--- src/main/java/net/kemitix/mon/reader/Reader.java
+++ src/main/java/net/kemitix/mon/reader/Reader.java
@@ -1,11 +1,15 @@
 package net.kemitix.mon.reader;
 
+import org.apiguardian.api.API;
+
 /**
  * Returns a program ready to run upon the supply of a suitable environment.
  *
  * @param <E> The type of the environment required by the program.
  * @param <R> The type of the result returned by the program.
  */
+@API(status = API.Status.EXPERIMENTAL, since = "3.0.1")
+@FunctionalInterface
 public interface Reader<E, R> {
 
     /**
