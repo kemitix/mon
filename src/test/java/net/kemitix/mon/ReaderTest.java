af0b241a46c7a43f94f27db6ade9acb0d8d5610c
Paul Campbell <pcampbell@kemitix.net>
2021-08-19 13:40:21 +0100

    Add Reader.map and Reader.andThen methods

--- src/test/java/net/kemitix/mon/ReaderTest.java
+++ src/test/java/net/kemitix/mon/ReaderTest.java
@@ -5,6 +5,8 @@ import org.assertj.core.api.WithAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class ReaderTest
         implements WithAssertions {
 
@@ -28,6 +30,49 @@ public class ReaderTest
 
         //then
         assertThat(result).isEqualTo(value);
+    }
+
+    @Test
+    @DisplayName("map")
+    void map() {
+        //given
+        AtomicInteger value = new AtomicInteger(123);
+        Environment env = new Environment() {
+            @Override
+            public Integer intValue() {
+                return value.incrementAndGet();
+            }
+        };
+        Reader<Environment, String> program =
+                ((Reader<Environment, Integer>) e -> e.intValue())
+                        .map(i -> i * 2)
+                        .map(i -> Integer.toString(i));
+        //when
+        String result = program.run(env);
+        //then
+        assertThat(result).isEqualTo("248");// (123 + 1) * 2
+    }
+
+    @Test
+    @DisplayName("andThen")
+    void andThen() {
+        //given
+        AtomicInteger value = new AtomicInteger(123);
+        Environment env = new Environment() {
+            @Override
+            public Integer intValue() {
+                return value.incrementAndGet();
+            }
+        };
+        Reader<Environment, String> program =
+                ((Reader<Environment, Integer>) e -> e.intValue())
+                        .andThen((e, i) -> e.intValue() + (i * 2))
+                        .andThen((e, i) -> String.format("%.2f",
+                                i.floatValue() / e.intValue()));
+        //when
+        String result = program.run(env);
+        //then
+        assertThat(result).isEqualTo("2.96");// ((123 * 2) + (123 + 1)) / (123 + 2)
     }
 
     private interface Environment {
